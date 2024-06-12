89c23fe396a69a3052c4dedbaa0290d0e711085a
Rozay Chen <rozayc@amazon.com>
2024-06-12 12:42:02 -0700

    Test 45 (more specific)

--- Amplify/Amplify.swift
+++ Amplify/Amplify.swift
@@ -125,7 +125,7 @@ public class TestClass{
         return oldVersion>newVersion-optionalVersion
     }
 }
-typealias UserName = Int
+typealias UserName = Double
 public class SubTestClass: TestClass{
     public override func testAPIBreakage(oldVersion: Int, newVersion: Int, optionalVersion: Int = 10) -> Bool {
         return oldVersion>newVersion+optionalVersion
