dd6aa7ea91f71e4a18017ec8d4114e5337db03e9
Rozay Chen <rozayc@amazon.com>
2024-06-12 11:47:12 -0700

    Revert "Test 25"
    This reverts commit cecd84faf46ac991f1df312df280f49e0b5c5f51.

--- Amplify/Amplify.swift
+++ Amplify/Amplify.swift
@@ -29,7 +29,6 @@ public class Amplify {
     static var isConfigured = false
     
     public static var testBreak1: Int = 4
-    public static var testBreak2: Int = 4
     public func testAPIBreakageNameChange(oldVersion: Int, newVersion: Int, optionalVersion: Int = 10) -> Bool {
         return oldVersion>newVersion-optionalVersion
     }
